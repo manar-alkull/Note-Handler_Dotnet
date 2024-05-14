5de3909d7cdbedec1f70fb02d76956a6cf486fbb
manar.alkul <manar.alkul@connamix.com>
2024-05-14 21:44:03 +0300

    add auth logic

--- NoteManager/Program.cs
+++ NoteManager/Program.cs
@@ -1,4 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NoteManager.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+//--------------------------------------------------------------------------------------------------------
+//https://dotnettutorials.net/lesson/asp-net-core-identity-setup/
+var connectionString = builder.Configuration.GetConnectionString("SQLServerIdentityConnection") ?? throw new InvalidOperationException("Connection string 'SQLServerIdentityConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+//--------------------------------------------------------------------------------------------------------
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,10 +35,20 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//--------------------------------------------------------------------------------------------------------
+
+//Configuring Authentication Middleware to the Request Pipeline
+app.UseAuthentication();
+//--------------------------------------------------------------------------------------------------------
+
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.MapRazorPages();
 
 app.Run();
