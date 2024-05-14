2bd68ee078e23fe3e76d9948b1cac6a8a14b7834
manar.alkul <manar.alkul@connamix.com>
2024-05-14 18:09:52 +0300

    new empty solution

---
+++ NoteManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoteManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
