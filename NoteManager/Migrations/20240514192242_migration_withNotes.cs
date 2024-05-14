6368db3827eea9689336f4c7514365353dc2adb8
manar.alkul <manar.alkul@connamix.com>
2024-05-14 22:24:34 +0300

    note migration

---
+++ NoteManager/Migrations/20240514192242_migration_withNotes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration_withNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
