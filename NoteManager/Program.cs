949d5895252026baf5ed83ecc44609eee36745a3
manar.alkul <manar.alkul@connamix.com>
2024-05-14 22:06:52 +0300

    migration and run (works)

--- NoteManager/Program.cs
+++ NoteManager/Program.cs
@@ -4,7 +4,8 @@ using NoteManager.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 //--------------------------------------------------------------------------------------------------------
 //https://dotnettutorials.net/lesson/asp-net-core-identity-setup/
