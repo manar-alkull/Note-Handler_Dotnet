9d89582db21f7bab9f97e4632724bff5c588fe41
manar.alkul <manar.alkul@connamix.com>
2024-05-21 09:47:47 +0300

    add migration4

---
+++ NoteManager/Migrations/20240521064712_migration4.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Public_Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Note = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Public_Links", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Public_Links_Notes_Note",
+                        column: x => x.Note,
+                        principalTable: "Notes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Public_Links_Note",
+                table: "Public_Links",
+                column: "Note");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Public_Links");
+        }
+    }
+}
